dcdd8363557c194c20d653358f4d3e7be684f776
RipperOnik <kingstrong2@mail.ru>
2023-09-23 20:11:42 -0400

    Updates

---
+++ precache-manifest.49f3a3f1d17ddfad4c5218acf0794a65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5e90fa0e7e0a9afc675",
+    "url": "/static/css/main.5f7202f9.chunk.css"
+  },
+  {
+    "revision": "d5e90fa0e7e0a9afc675",
+    "url": "/static/js/main.d5e90fa0.chunk.js"
+  },
+  {
+    "revision": "8548251f02429b08224f",
+    "url": "/static/js/1.8548251f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7250e5cf22ab4d5df17ed94786213d41",
+    "url": "/index.html"
+  }
+];
