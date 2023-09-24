af73f3ee02502602003fbbf6c44cdddad7f503c6
RipperOnik <kingstrong2@mail.ru>
2023-09-23 20:16:19 -0400

    Updates

---
+++ precache-manifest.dc7bd3439c615f7c0008736e15ab43d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5e90fa0e7e0a9afc675",
+    "url": "/retail-store-rest-frontend/static/css/main.5f7202f9.chunk.css"
+  },
+  {
+    "revision": "d5e90fa0e7e0a9afc675",
+    "url": "/retail-store-rest-frontend/static/js/main.d5e90fa0.chunk.js"
+  },
+  {
+    "revision": "8548251f02429b08224f",
+    "url": "/retail-store-rest-frontend/static/js/1.8548251f.chunk.js"
+  },
+  {
+    "revision": "e0aa8948cd3a68f4e420",
+    "url": "/retail-store-rest-frontend/static/js/runtime~main.e0aa8948.js"
+  },
+  {
+    "revision": "169b572ff97ffc1ed13e975ef65cb164",
+    "url": "/retail-store-rest-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49f3a3f1d17ddfad4c5218acf0794a65.js"
+  "/retail-store-rest-frontend/precache-manifest.dc7bd3439c615f7c0008736e15ab43d7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/retail-store-rest-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
